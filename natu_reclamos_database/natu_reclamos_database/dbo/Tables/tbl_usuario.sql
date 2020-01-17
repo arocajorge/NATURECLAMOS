64d8627e963cb6d81eb54860062319a6b41743e7
WendyCherres <wendycherres@gmail.com>
2020-01-17 13:47:56 -0500

    ..

--- natu_reclamos_database/natu_reclamos_database/dbo/Tables/tbl_usuario.sql
+++ natu_reclamos_database/natu_reclamos_database/dbo/Tables/tbl_usuario.sql
@@ -2,10 +2,12 @@
     [IdUsuario]      VARCHAR (50)  NOT NULL,
     [us_contrasenia] VARCHAR (200) NOT NULL,
     [us_nombre]      VARCHAR (500) NULL,
-    [us_estado]      BIT           NOT NULL,
     [us_tipo]        VARCHAR (50)  NULL,
+    [us_estado]      BIT           NOT NULL,
     CONSTRAINT [PK_tbl_usuario] PRIMARY KEY CLUSTERED ([IdUsuario] ASC)
 );
+
+
 
 
 
